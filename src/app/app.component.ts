d530aa7d929f0929565be5b9f6ae131f02c5a9d4
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-05-15 16:28:11 +0900

    add http fetch code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
+
+    <p>User: {{ user().name }}</p>
 
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'apphosting-sandbox';
+  title = "apphosting-sandbox";
+
+  user = signal({ name: "John Doe" });
+
+  #httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.#httpClient
+      .get<{ name: string }>("https://jsonplaceholder.typicode.com/users/1")
+      .subscribe((user) => {
+        this.user.set(user);
+      });
+  }
 }
